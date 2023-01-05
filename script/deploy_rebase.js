b78490ebe41e52150556eef3b7f0d7a7bd98ed52
Kefa698 <kefaombongi07@gmail.com>
2023-01-06 01:08:31 +0300

    first commit

---
+++ script/deploy_rebase.js
@@ -0,0 +1,14 @@
+async function main() {
+    const Accumulator = await ethers.getContractFactory("RebasePair")
+    
+    const accumulator = await Accumulator.deploy()
+    await accumulator.deployed()
+    console.log("Contract deployed to address:", accumulator.address)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
